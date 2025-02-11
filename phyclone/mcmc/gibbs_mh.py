cf2db38334fdc50196688021d49e5b05931846d6
elhurtado <74883583+elhurtado@users.noreply.github.com>
2025-02-11 06:26:30 -0800

    Type hinting and minor efficiency tweaks

--- phyclone/mcmc/gibbs_mh.py
+++ phyclone/mcmc/gibbs_mh.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from phyclone.tree import Tree, TreeJointDistribution
 from phyclone.utils.math import exp_normalize, log_normalize
 
 
@@ -11,14 +11,14 @@ class DataPointSampler(object):
 
     __slots__ = ("tree_dist", "outliers", "_rng")
 
-    def __init__(self, tree_dist, rng: np.random.Generator, outliers=False):
+    def __init__(self, tree_dist: TreeJointDistribution, rng: np.random.Generator, outliers: bool = False):
         self.tree_dist = tree_dist
 
         self.outliers = outliers
 
         self._rng = rng
 
-    def sample_tree(self, tree):
+    def sample_tree(self, tree: Tree):
         tree_labels = tree.labels
         data_idxs = list(tree_labels.keys())
 
@@ -32,38 +32,89 @@ class DataPointSampler(object):
 
         return tree
 
-    def _sample_tree(self, data_idx, tree, old_node):
+    # @profile
+    # def _sample_tree(self, data_idx, tree, old_node):
+    #     data_point = tree.data[data_idx]
+    #
+    #     assert data_point.idx == data_idx
+    #
+    #     new_trees = []
+    #
+    #     for new_node in tree.nodes:
+    #         new_tree = tree.copy()
+    #
+    #         new_tree.remove_data_point_from_node(data_point, old_node)
+    #
+    #         new_tree.add_data_point_to_node(data_point, new_node)
+    #
+    #         new_trees.append(new_tree)
+    #
+    #     if self.outliers:
+    #         new_tree = tree.copy()
+    #
+    #         new_tree.remove_data_point_from_node(data_point, old_node)
+    #
+    #         new_tree.add_data_point_to_outliers(data_point)
+    #
+    #         new_trees.append(new_tree)
+    #
+    #     log_q = np.array([self.tree_dist.log_p_one(x) for x in new_trees])
+    #
+    #     log_q = log_normalize(log_q)
+    #
+    #     q = np.exp(log_q)
+    #
+    #     q = q / sum(q)
+    #
+    #     tree_idx = self._rng.multinomial(1, q).argmax()
+    #
+    #     return new_trees[tree_idx]
+
+    def _sample_tree(self, data_idx: int, tree: Tree, old_node: int | str) -> Tree:
         data_point = tree.data[data_idx]
 
         assert data_point.idx == data_idx
 
+        tree_nodes = tree.nodes
+
+        len_new_trees = len(tree_nodes)
+        if self.outliers:
+            len_new_trees += 1
+
+        # new_trees = np.empty(len_new_trees, dtype=object)
         new_trees = []
+        log_q = np.empty(len_new_trees, dtype=np.float64, order='C')
 
-        for new_node in tree.nodes:
-            new_tree = tree.copy()
+        rem_tree = tree.copy()
+        rem_tree.remove_data_point_from_node(data_point, old_node)
 
-            new_tree.remove_data_point_from_node(data_point, old_node)
+        tree_dist = self.tree_dist
+
+        for i, new_node in enumerate(tree_nodes):
+            new_tree = rem_tree.copy()
 
             new_tree.add_data_point_to_node(data_point, new_node)
 
+            # new_trees[i] = new_tree
             new_trees.append(new_tree)
+            log_q[i] = tree_dist.log_p_one(new_tree)
 
         if self.outliers:
-            new_tree = tree.copy()
-
-            new_tree.remove_data_point_from_node(data_point, old_node)
+            new_tree = rem_tree.copy()
 
             new_tree.add_data_point_to_outliers(data_point)
 
+            # new_trees[-1] = new_tree
             new_trees.append(new_tree)
+            log_q[-1] = tree_dist.log_p_one(new_tree)
 
-        log_q = np.array([self.tree_dist.log_p_one(x) for x in new_trees])
+        assert len(new_trees) == len(log_q)
 
         log_q = log_normalize(log_q)
 
-        q = np.exp(log_q)
+        q = np.exp(log_q, out=log_q)
 
-        q = q / sum(q)
+        q /= q.sum()
 
         tree_idx = self._rng.multinomial(1, q).argmax()
 
@@ -75,12 +126,12 @@ class PruneRegraphSampler(object):
 
     __slots__ = ("tree_dist", "_rng")
 
-    def __init__(self, tree_dist, rng: np.random.Generator):
+    def __init__(self, tree_dist: TreeJointDistribution, rng: np.random.Generator):
         self.tree_dist = tree_dist
 
         self._rng = rng
 
-    def sample_tree(self, tree):
+    def sample_tree(self, tree: Tree):
         if tree.get_number_of_nodes() <= 1:
             return tree
 
@@ -100,7 +151,7 @@ class PruneRegraphSampler(object):
         return trees[idx][1]
 
     @staticmethod
-    def _create_sampled_trees_array(remaining_nodes, pruned_tree, subtree):
+    def _create_sampled_trees_array(remaining_nodes: list[int | str | None], pruned_tree: Tree, subtree: Tree) -> list[tuple[int, Tree]]:
         trees = []
         # Descendant from dummy normal node
         remaining_nodes.append(None)
@@ -117,10 +168,10 @@ class PruneRegraphSampler(object):
 
             new_tree.update()
 
-            trees.append([nc, new_tree])
+            trees.append((nc, new_tree))
         return trees
 
-    def _get_subtree_and_pruned_tree(self, tree):
+    def _get_subtree_and_pruned_tree(self, tree: Tree) -> tuple[list[int | str | None], Tree, Tree]:
         pruned_tree = tree.copy()
         subtree_root = self._rng.choice(pruned_tree.nodes)
         subtree = pruned_tree.get_subtree(subtree_root)
