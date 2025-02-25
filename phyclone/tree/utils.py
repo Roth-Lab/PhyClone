5023d143aba4529895b00722500fe7f746542cf5
elhurtado <74883583+elhurtado@users.noreply.github.com>
2025-02-25 13:59:12 -0800

    moving out of log space once during compute_log_D

--- phyclone/tree/utils.py
+++ phyclone/tree/utils.py
@@ -31,36 +31,6 @@ def compute_log_S(child_log_R_values):
 #     return log_S
 
 
-# def compute_log_D(child_log_R_values):
-#     num_children = len(child_log_R_values)
-#
-#     if num_children == 0:
-#         return 0
-#
-#     if num_children == 1:
-#         return child_log_R_values[0]
-#
-#     all_maxes = np.max(child_log_R_values, axis=-1, keepdims=True)
-#     normed_children = np.empty_like(child_log_R_values, order="C")
-#
-#     np.subtract(child_log_R_values, all_maxes, out=normed_children)
-#
-#     np.exp(normed_children, out=normed_children)
-#
-#     normed_children = np.ascontiguousarray(normed_children)
-#
-#     conv_res = _convolve_two_children(normed_children[0], normed_children[1])
-#     for j in range(2, num_children):
-#         conv_res = _convolve_two_children(normed_children[j], conv_res)
-#
-#     conv_res[conv_res <= 0] = 1e-100
-#
-#     np.log(conv_res, dtype=np.float64, out=conv_res)
-#
-#     conv_res += all_maxes.sum(0)
-#
-#     return conv_res
-
 def compute_log_D(child_log_R_values):
     num_children = len(child_log_R_values)
 
@@ -70,14 +40,51 @@ def compute_log_D(child_log_R_values):
     if num_children == 1:
         return child_log_R_values[0]
 
-    # child_log_R_values = np.ascontiguousarray(child_log_R_values)
+    all_maxes = np.max(child_log_R_values, axis=-1, keepdims=True)
+    normed_children = np.empty_like(child_log_R_values, order="C")
 
-    conv_res = _convolve_two_children(child_log_R_values[0], child_log_R_values[1])
+    np.subtract(child_log_R_values, all_maxes, order="C", dtype=np.float64, out=normed_children)
+
+    np.exp(normed_children, order="C", dtype=np.float64, out=normed_children)
+
+    # normed_children = np.ascontiguousarray(normed_children)
+
+    conv_res = _convolve_two_children(normed_children[0], normed_children[1])
     for j in range(2, num_children):
-        conv_res = _convolve_two_children(child_log_R_values[j], conv_res)
+        conv_res = _convolve_two_children(normed_children[j], conv_res)
 
-    log_D = conv_res
-    return log_D
+    # conv_res[conv_res <= 0] = 1e-100
+    log_d = conv_res.copy()
+    log_d[log_d <= 0] = 1e-100
+    # log_d = np.clip(conv_res, a_min=1e-100, a_max=None, out=log_d)
+    # conv_res.clip(min=1e-100, max=None, order="C", dtype=np.float64, out=log_d)
+    # np.maximum(1e-100, conv_res, order="C", dtype=np.float64, out=log_d)
+
+    np.log(log_d, order="C", dtype=np.float64, out=log_d)
+
+    log_d += all_maxes.sum(0)
+
+    return log_d
+
+
+
+# def compute_log_D(child_log_R_values):
+#     num_children = len(child_log_R_values)
+#
+#     if num_children == 0:
+#         return 0
+#
+#     if num_children == 1:
+#         return child_log_R_values[0]
+#
+#     # child_log_R_values = np.ascontiguousarray(child_log_R_values)
+#
+#     conv_res = _convolve_two_children(child_log_R_values[0], child_log_R_values[1])
+#     for j in range(2, num_children):
+#         conv_res = _convolve_two_children(child_log_R_values[j], conv_res)
+#
+#     # log_D = conv_res
+#     return conv_res
 
 
 @two_np_arr_cache(maxsize=2048)
@@ -85,7 +92,10 @@ def _convolve_two_children(child_1, child_2):
     grid_size = child_1.shape[-1]
     if grid_size < 1000:
         # res_arr = conv_over_dims(child_1, child_2, np.zeros_like(child_1, order="C"))
-        res_arr = _np_conv_dims(child_1, child_2)
+        # res_arr = _np_conv_dims(child_1, child_2)
+        res_arr = np_conv_dims(child_1, child_2)
+        # res_arr.clip(min=1e-100, max=None, out=res_arr)
+        # res_arr[res_arr <= 0] = 1e-100
     else:
         res_arr = np.ascontiguousarray(fft_convolve_two_children(child_1, child_2))
     return res_arr
