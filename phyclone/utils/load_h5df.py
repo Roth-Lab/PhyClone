6d77b028846ae434f69c840a7e9fcda7df1e4bba
elhurtado <74883583+elhurtado@users.noreply.github.com>
2025-11-06 16:17:47 -0800

    Implementing load_clusters_df_from_trace to load clusters dataframe from trace

--- phyclone/utils/load_h5df.py
+++ phyclone/utils/load_h5df.py
@@ -18,3 +18,16 @@ def load_chain_trace_data_df(in_file):
 
     final_df = pd.concat(chain_df_list, ignore_index=True)
     return final_df
+
+
+def load_clusters_df_from_trace(in_file):
+    df_dict = dict()
+    with h5py.File(in_file) as fh:
+        clusters_grp = fh["clusters"]
+        # df_dict = {k: v[()] for k, v in clusters_grp.items()}
+        #TODO: set this up to be more flexible on type, IDs could be strings or numbers for any of these
+        df_dict["cluster_id"] = clusters_grp["cluster_id"][()]
+        df_dict["mutation_id"] = clusters_grp["mutation_id"][()].astype('T')
+
+    df = pd.DataFrame(df_dict)
+    return df
