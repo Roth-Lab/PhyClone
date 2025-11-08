b07ffb628638d648e8fdad26b3d415e33c922901
elhurtado <74883583+elhurtado@users.noreply.github.com>
2025-11-07 18:28:00 -0800

    No longer relabelling nodes during run, will be done during trace processing

--- phyclone/run.py
+++ phyclone/run.py
@@ -295,7 +295,7 @@ def _run_main_sampler(
             for _ in range(num_samples_prune_regraph):
                 tree = prg_sampler.sample_tree(tree)
 
-            tree.relabel_nodes()
+            # tree.relabel_nodes()
 
             if i % thin == 0:
                 append_to_trace(i, timer, trace, tree, tree_dist)
@@ -379,7 +379,7 @@ def _run_burnin(
                 for _ in range(num_samples_prune_regraph):
                     tree = prg_sampler.sample_tree(tree)
 
-                tree.relabel_nodes()
+                # tree.relabel_nodes()
 
                 tree_score = tree_dist.log_p_one(tree)
                 if tree_score > best_score:
